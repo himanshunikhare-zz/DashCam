76be92df329f5ab6c5b968a03a9658c752dfb52b
Himanshu Nikhare <himanshunikhare@outlook.com>
2019-05-01 10:22:30 +0530

    Added TextureView for displaying preview.

--- app/src/main/java/com/example/dash_cam/MainActivity.java
+++ app/src/main/java/com/example/dash_cam/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.dash_cam;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+//    To display camera stream to texture view
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            Toast.makeText(getApplicationContext(), "TextureView is available", Toast.LENGTH_SHORT).show();
+        }
 
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextureView = (TextureView) findViewById(R.id.textureView);
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mTextureView.isAvailable()) {
+        } else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
+//  To get full screen UI
     @Override
     public void onWindowFocusChanged(boolean hasFocus){
         super.onWindowFocusChanged(hasFocus);
